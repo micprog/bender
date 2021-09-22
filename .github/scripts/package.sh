f6109d063eb1b88dada636819b6dba76be246266
Michael Rogenmoser <michael@rogenmoser.us>
2021-09-22 17:43:19 +0200

    Add release action

---
+++ .github/scripts/package.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [[ "$GITHUB_REF" =~ ^v.*$ ]]; then
+    readonly pkgver="$(echo $GITHUB_REF | sed -n 's/^v//p')"
+else
+    readonly pkgver="$GITHUB_REF"
+fi
+
+if [ -z "$1" ]; then
+    readonly release_dir="target/release"
+    readonly tar_suffix=""
+else
+    readonly release_dir="target/$1/release"
+    readonly tar_suffix="-$1"
+fi
+
+tar -czf "bender-$pkgver-x86_64-linux-gnu$tar_suffix.tar.gz" \
+        -C "./$release_dir" \
+        --owner=0 --group=0 \
+        bender
